228aa0548e58d9b0f1552bc5315728b99b22b007
Gnarfoz (Christian Wiese) <gnarfoz@gmail.com>
2012-11-27 00:20:52 +0100

    SetDungeonDifficulty was renamed to SetDungeonDifficultyID

--- picoGroup.lua
+++ picoGroup.lua
@@ -192,7 +192,7 @@ function dataobj:OnClick(button)
 	if not dropdown then
 		dropdown = CreateFrame("Frame", "picoGroupDownFrame", self, "UIDropDownMenuTemplate")
 
-		local function sdd(self) SetDungeonDifficulty(self.value) end
+		local function sdd(self) SetDungeonDifficultyID(self.value) end
 		local function srd(self)
 			SetRaidDifficulty(self.value)
 			if not IsInRaid() then ConvertToRaid() end
